3f41aefd2120d989d4ed1855a74bfe980bb75a1c
phheerakorns-bit <phheerakorns@gmail.com>
2026-09-17 00:18:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqSRNOKusRDdnvEAMmUGPUIfErLHgXAsyi52xsYB_lNRMxl-GA6X4zOgEyS244oFsj/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwRYTofrqiijrsYg2A-hIBflqWr6Lpkg-v9ejn0RpuzxNztDGyowtHiinOZMD1flxoC/exec';
 const CSV_URL = 'YOUR_CSV_URL';
 
 document.addEventListener('DOMContentLoaded', () => {
